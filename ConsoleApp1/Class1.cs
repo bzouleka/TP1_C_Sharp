cf70c5052bddc889167dfba04ff63f099181236b
Zouzou 86 <bzouleka@gmail.com>
2018-06-19 13:37:50 +0200

    first TP C#

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Message
+    {
+        public void AfficherBonWeekEnd()
+
+        {
+
+            Console.WriteLine("Bon week-end " + Environment.UserName);
+            Console.ReadKey();
+
+        }
+
+
+        public void AfficherBonjour()
+
+        {
+
+            Console.WriteLine("Bonjour " + Environment.UserName);
+            Console.ReadKey();
+
+        }
+
+
+        public void AfficherBonneApresMidi()
+
+        {
+
+            Console.WriteLine("Bonne après-midi " + Environment.UserName);
+            Console.ReadKey();
+
+        }
+
+        public void AfficherBonsoir()
+
+        {
+
+            Console.WriteLine("Bonsoir " + Environment.UserName);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static int Date { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Message message = new Message();
+
+            var now = new DateTime(2018, 6, 17, 18, 12,32);
+
+            if (now.DayOfWeek == DayOfWeek.Saturday || now.DayOfWeek == DayOfWeek.Sunday)
+
+            {
+                
+                message.AfficherBonWeekEnd();
+
+            }
+
+            else
+
+            {
+                
+                if (now.DayOfWeek == DayOfWeek.Monday && now.Hour < 9)
+
+                {
+                    
+                    message.AfficherBonWeekEnd();
+
+                }
+
+                else
+
+                {
+
+                    if (now.Hour >= 9 && now.Hour < 13)
+
+                    {
+                        
+                        message.AfficherBonjour();
+
+                    } else if (now.Hour >= 13 && now.Hour < 18)
+                    {
+                        message.AfficherBonneApresMidi();
+                    }
+
+                    else
+
+                    {
+                        if (now.DayOfWeek == DayOfWeek.Friday && now.Hour >= 18)
+
+                        {
+
+                            message.AfficherBonWeekEnd();
+
+                        }
+
+                        else
+
+                        {
+
+                            message.AfficherBonsoir();
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+
+
+        
+
+
+    }
+}
