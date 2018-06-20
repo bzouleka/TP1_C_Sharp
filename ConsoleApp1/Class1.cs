80ee2d6d3e77d5aabab105929e966e7812befe18
bzouleka <bzouleka@gmail.com>
2018-06-20 09:39:39 +0200

    m

--- ConsoleApp1/Class1.cs
+++ ConsoleApp1/Class1.cs
@@ -6,43 +6,111 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+
+
     class Message
     {
-        public void AfficherBonWeekEnd()
 
+        private DateTime now = DateTime.Now;
+        private int morning, afternoon, night;
+        
+
+         public Message()
         {
 
-            Console.WriteLine("Bon week-end " + Environment.UserName);
-            Console.ReadKey();
+        }
+        // constructeur de paramètre mes jours de la semaine
+        public Message(DateTime now)
+        {
+
+            this.now = now;
+
+        }
+        //constructeur des paramètre de la date et des tranches horaire
+        public Message(DateTime now ,  int morning = 9, int afternoon = 13, int night = 18 )
+        {
+
+            this.morning = morning;
+            this.afternoon = afternoon;
+            this.night = night;
+            this.now = now;
 
         }
 
 
-        public void AfficherBonjour()
+        public string GetHelloMessage()
+    {
+
+         
+
+        if (now.DayOfWeek == DayOfWeek.Saturday || now.DayOfWeek == DayOfWeek.Sunday)
 
         {
 
-            Console.WriteLine("Bonjour " + Environment.UserName);
-            Console.ReadKey();
-
+            return "Bon week-end ";
+           
         }
 
-
-        public void AfficherBonneApresMidi()
+        else
 
         {
 
-            Console.WriteLine("Bonne après-midi " + Environment.UserName);
-            Console.ReadKey();
+            if (now.DayOfWeek == DayOfWeek.Monday && now.Hour <morning)
+
+            {
+
+                return "Bon week-end ";
+               
+
+            }
+
+            else
+
+            {
+
+                if (now.Hour >=morning && now.Hour < afternoon)
+
+                {
+
+                    return "Bonjour ";
+                   
+
+                }
+                else if (now.Hour >= afternoon && now.Hour < night)
+                {
+                    return "Bonne après-midi ";
+                    
+                }
+
+
+                else
+
+                {
+                    if (now.DayOfWeek == DayOfWeek.Friday && now.Hour >= night)
+
+                    {
+
+                        return "Bon week-end " ;
+                        
+
+                    }
+
+                    else
+
+                    {
+
+                        return "Bonsoir " ;
+                        
+
+                    }
+
+                }
+
+            }
 
         }
 
-        public void AfficherBonsoir()
-
-        {
-
-            Console.WriteLine("Bonsoir " + Environment.UserName);
-            Console.ReadKey();
-        }
     }
+}
+
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,75 +12,17 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            Message message = new Message();
+            var now = new DateTime(2018, 6, 18, 18, 00, 32);
+            Message message = new Message(now, 8, 12, 19);
+            
 
-            var now = new DateTime(2018, 6, 17, 18, 12,32);
+            Console.WriteLine(message.GetHelloMessage() + " " + Environment.UserName);
+            Console.ReadKey();
 
-            if (now.DayOfWeek == DayOfWeek.Saturday || now.DayOfWeek == DayOfWeek.Sunday)
 
-            {
-                
-                message.AfficherBonWeekEnd();
 
-            }
 
-            else
-
-            {
-                
-                if (now.DayOfWeek == DayOfWeek.Monday && now.Hour < 9)
-
-                {
-                    
-                    message.AfficherBonWeekEnd();
-
-                }
-
-                else
-
-                {
-
-                    if (now.Hour >= 9 && now.Hour < 13)
-
-                    {
-                        
-                        message.AfficherBonjour();
-
-                    } else if (now.Hour >= 13 && now.Hour < 18)
-                    {
-                        message.AfficherBonneApresMidi();
-                    }
-
-                    else
-
-                    {
-                        if (now.DayOfWeek == DayOfWeek.Friday && now.Hour >= 18)
-
-                        {
-
-                            message.AfficherBonWeekEnd();
-
-                        }
-
-                        else
-
-                        {
-
-                            message.AfficherBonsoir();
-
-                        }
-
-                    }
-
-                }
-
-            }
 
         }
-
-
-        
-
-
     }
 }
