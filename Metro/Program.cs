a7791e558f76913faf690f965d5a3bc13692bca7
bzouleka <bzouleka@gmail.com>
2018-06-20 15:56:47 +0200

    it 2

--- Metro/Program.cs
+++ Metro/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using static System.Console;
+using Newtonsoft.Json;
 
 namespace Metro
 {
@@ -10,6 +14,38 @@ namespace Metro
     {
         static void Main(string[] args)
         {
+
+            // Create a request for the URL.   
+            WebRequest request = WebRequest.Create(
+              "https://data.metromobilite.fr/api/linesNear/json?x=5.709360123&y=45.176494599999984&dist=400&details=true");           
+            // Get the response.  
+            WebResponse response = request.GetResponse();
+            // Display the status.  
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.  
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.  
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.  
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content. 
+
+            List<Lines> lines = JsonConvert.DeserializeObject<List<Lines>>(responseFromServer);
+            List<Lines> listSansDoublon = lines.GroupBy(i => i.name).Select(g => g.First()).ToList();
+            
+
+            foreach (Lines line in listSansDoublon) // iteration ligne par ligne 
+            {
+            Console.WriteLine($"Nom de la ligne {line.name}");
+                
+            }
+
+            // Clean up the streams and the response.  
+            
+            reader.Close();
+            response.Close();
+            ReadLine(); // sinon le programme sors sans afficher de resultats
+
         }
     }
 }
---
+++ Metro/lines.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metro
+{
+    class Lines
+    {
+        // fonctions generer par jsontocsharp a partir du json de metromobilité
+        public string id { get; set; }
+        public object ID { get; internal set; }
+        public string name { get; set; }
+        public double lon { get; set; }
+        public double lat { get; set; }
+        public List<string> lines { get; set; }
+    }
+}
